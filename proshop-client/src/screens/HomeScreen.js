0e1a383f350db7c4e0027e8fd6fae5d855f4a603
mazenswar <mazenswar@protonmail.com>
2021-07-23 08:19:00 -0400

    added backend routing

--- proshop-client/src/screens/HomeScreen.js
+++ proshop-client/src/screens/HomeScreen.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import products from '../products';
+import React, { useEffect, useState } from 'react';
+// import products from '../products';
 import { Col, Row } from 'react-bootstrap';
 import Product from '../components/Product';
+import axios from 'axios';
 
 function HomeScreen() {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        async function fetchProducts() {
+            const { data } = await axios.get('/api/products/');
+            setProducts(data);
+        }
+        fetchProducts();
+    }, [])
     return (
         <>
             <h1>Latest Products</h1>
